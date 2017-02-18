6a26820b819a5159e03c1edb8cd38d23799c1b94
shtigran <shakhbekyantigran@gmail.com>
2017-02-18 20:50:15 +0400

    Explanation of Nullable<T>

--- Nullable Types/Nullable Types/Program.cs
+++ Nullable Types/Nullable Types/Program.cs
@@ -11,7 +11,12 @@ namespace Nullable_Types
         static void Main(string[] args)
         {
 
-            int? a = null;  // Nullable type for valutype
+            int? a = null;
+            
+            // Nullable type for valutype
+            // Nullable types create instance from Nullable<T> generic type. And inherits the Hasvalue and Value properties.
+            // That Hasvalue return boolean type, and indicate is the variable has value or not. And the Value propery allow take the value of 
+            // variable or give the value
         }
     }
 }
